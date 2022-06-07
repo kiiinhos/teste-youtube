057f426154d6a7130e9c308dcfb5e2071bda8ff0
viniciuscamargo713@gmail.com <viniciuscamargo713@gmail.com>
2022-06-07 18:13:52 -0300

    configurando material-ui

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export default function Home() {
+  return (
+    <div>
+      Clone youtube
+      <Button variant="contained" color="primary">Contained</Button>
+    </div>
+    
+  );
+}
